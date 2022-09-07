44fe920e9653b0308a9316c9d14502da15b7e929
lavr174 <lavr174@icloud.com>
2022-09-07 19:33:28 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int numbersNatur (int M, int N)
+{
+    if ( M == N) return N;
+    return M + numbersNatur(M+1, N);
+}
+
+Console.WriteLine("Введите первое число ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = numbersNatur (M, N);
+Console.WriteLine($"Сумма равна = {sum}");
